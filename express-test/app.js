7e057685d4ec3ddfabf0d3d5dfd90de70c00d7d8
LorriAnn <LorriAnn7@att.net>
2021-08-12 06:22:15 -0400

    directs to about page

--- express-test/app.js
+++ express-test/app.js
@@ -4,15 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var homeRouter = require('./routes/home');
-var aboutRouter = require('./routes/about');
-var contactMeRouter = require('./routes/contact-me');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 
 var app = express();
-var router = express.Router();
 
 // view engine setup
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -22,13 +19,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', homeRouter);
-app.use('/about', aboutRouter);
-app.use('/contact-me', contactMeRouter);
-
-router.get('/about', function(req, res, next) {
-  res.send('About page');
-})
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/contact-me.js
+++ routes/contact-me.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/contact-me', function(req, res, next) {
-  res.send('contact me');
+router.get('/', function(req, res, next) {
+  res.send('Contact Me');
 });
 
 module.exports = router;
