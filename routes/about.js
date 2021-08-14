9dff0fe1ae325abcdb752870ca41fa22a64f7db2
LorriAnn <LorriAnn7@att.net>
2021-08-13 22:23:41 -0400

    directs pages to templete

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('About');
+  // res.send('About');
+  res.render('about', { title: 'Express' });
 });
 
 module.exports = router;
--- routes/contact-me.js
+++ routes/contact-me.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Contact Me');
+  res.render('contact-me', { title: 'Express' });
 });
 
 module.exports = router;
